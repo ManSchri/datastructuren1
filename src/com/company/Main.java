e85df16cfb6e3294de111a703345f6fefe6794c2
Denzel <dmaria95@hotmail.com>
2016-03-11 17:08:28 +0100

    PUSH!!!!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Object typos;
         WebsitesTrie trie = new WebsitesTrie("URLs.txt");
         UserInput input = new UserInput();
         Object[][] foundUrls = trie.search(input.url);
+        typos = foundUrls[0][1];
         if(foundUrls[0][0] == null){
             System.out.println("No results found");
         }
-        //else if (trie.typos > 0){
+        else if ((int) typos == 0) {
+            System.out.println("Result found!");
+        }
+        else {
             System.out.println("Did you mean: ");
-            for (int i=0; i<foundUrls.length; i++) {
+            for (int i=0; i<5; i++) {
+
                 if (foundUrls[i][0] == null) {
                     break;
                 }
-                System.out.println(foundUrls[i][0]);
-                System.out.println(foundUrls[i][1]);
+                else {
+                    System.out.println(foundUrls[i][0]);
+                    // System.out.println(foundUrls[i][1]);
+                }
             }
-       // }
-       // else {
-         //   System.out.println("Result found!");
-       // }
-
+        }
     }
 }
